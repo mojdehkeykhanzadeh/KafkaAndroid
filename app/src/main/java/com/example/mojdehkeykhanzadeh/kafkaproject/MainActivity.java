f37a8f2fe263cc253075bfa06a1059f12e006bf3
Mojdeh Keykhanzadeh <mojdeh.keykhanzadeh@sjsu.edu>
2018-12-06 21:46:33 -0800

    fixed to work with 3 topics

--- app/src/main/java/com/example/mojdehkeykhanzadeh/kafkaproject/MainActivity.java
+++ app/src/main/java/com/example/mojdehkeykhanzadeh/kafkaproject/MainActivity.java
@@ -39,27 +39,38 @@ private String topic3;
             @Override
             public void onClick(View v) {
                 Toast.makeText(getBaseContext(), "Stream started", Toast.LENGTH_SHORT).show();
-                saveTopic();
+                saveTopic1();
             }
         });
+        Button btnSave2= (Button) findViewById(R.id.buttonSend2);
+        btnSave2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Stream started", Toast.LENGTH_SHORT).show();
+                saveTopic2();
+            }
+        });
+        Button btnSave3= (Button) findViewById(R.id.buttonSend3);
+        btnSave3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Stream started", Toast.LENGTH_SHORT).show();
+                saveTopic3();
+            }
+        });
+
     }
-    public void saveTopic(){
-        String URL = "endpoint";
+    public void saveTopic1(){
+        String URL = "endpoint1";
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         text1 = findViewById(R.id.editText1);
-        text2 = findViewById(R.id.editText2);
-        text3 = findViewById(R.id.editText3);
         topic1 = text1.getText().toString();
-        topic2 = text2.getText().toString();
-        topic3=text3.getText().toString();
 
         StringRequest objectRequest = new StringRequest(Request.Method.POST, URL,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         Log.e("Response", response);
-                      //  Toast.makeText(MainActivity.this,response,Toast.LENGTH_LONG).
-                               // getGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0).show();
                         Toast toast= Toast.makeText(getApplicationContext(),
                                 response, Toast.LENGTH_LONG);
                         toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
@@ -76,13 +87,82 @@ private String topic3;
                         // this is the relevant method
                         @Override
                         public byte[] getBody() {
-                            String httpPostBody=topic1 +"\r\n"+ topic2 +"\r\n"+ topic3;
+                            String httpPostBody=topic1 ;
                             return httpPostBody.getBytes();
                         }
 
                 };
         requestQueue.add(objectRequest);
     }
+    public void saveTopic2(){
+        String URL = "endpoint2";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        text2 = findViewById(R.id.editText2);
+        topic2 = text2.getText().toString();
+
+        StringRequest objectRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("Response", response);
+                        Toast toast= Toast.makeText(getApplicationContext(),
+                                response, Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                        toast.show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+                    }
+                }) {
+            // this is the relevant method
+            @Override
+            public byte[] getBody() {
+                String httpPostBody=topic2 ;
+                return httpPostBody.getBytes();
+            }
+
+        };
+        requestQueue.add(objectRequest);
+    }
+    public void saveTopic3(){
+        String URL = "endpoint3";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        text3 = findViewById(R.id.editText3);
+        topic3=text3.getText().toString();
+
+        StringRequest objectRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("Response", response);
+                        Toast toast= Toast.makeText(getApplicationContext(),
+                                response, Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                        toast.show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+                    }
+                }) {
+            // this is the relevant method
+            @Override
+            public byte[] getBody() {
+                String httpPostBody= topic3;
+                return httpPostBody.getBytes();
+            }
+
+        };
+        requestQueue.add(objectRequest);
+    }
+
 
 
 }
